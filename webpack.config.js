538a154d6f59ffd5ca22bf384f84263817beb59e
PaulCathey <pmcathey@gmail.com>
2016-11-14 11:51:06 -0800

    Begin animation

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './down_beat.js',
+  output: {
+    path: path.join(__dirname, 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
